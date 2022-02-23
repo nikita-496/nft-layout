b70198d88de5376b9c74675cd93f2c4b25f7fb3d
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-02-23 14:25:48 +0500

    overwriting the implementation of the select on the module

--- src/blocks/select/select.js
+++ src/blocks/select/select.js
@@ -1,40 +1,26 @@
-let isShow = false;
-
-const selectToggle = document.querySelector('.select__toggle');
-const selectDropdown = document.querySelector('.select__dropdown');
-
-const selectOption = document.querySelector('.select__options');
-
-selectToggle.addEventListener('click', changeStateDropDown);
-selectOption.addEventListener('click', setOption);
-
-function changeStateDropDown() {
-  isShow = !isShow;
-  if (isShow) {
-    selectDropdown.style.display = 'block';
-  } else {
-    selectDropdown.dataShow = isShow;
-    selectDropdown.style.display = 'none';
+EntryPoint.select = (function () {
+  let activelectToggle = null;
+  function getDropdownState() {
+    const listSelectToggle = getSelectToggle();
+    listSelectToggle.forEach((selectToggle) => {
+      selectToggle.addEventListener('click', () => changeStateDropDown(selectToggle));
+      selectToggle.addEventListener('click', () => (activelectToggle = selectToggle));
+    });
   }
-}
 
-function setOption(event) {
-  const trendingOptions = ['Games', 'View', 'Music'];
-  const target = event.target;
-  //если пользователь кликнул по элементу ul
-  if (target.className === 'select__options') return;
-
-  setActiveClass(target);
-  selectToggle.value = target.dataset.value;
-  selectToggle.dataset.index = target.dataset.index;
-  selectToggle.innerHTML = trendingOptions[selectToggle.dataset.index];
-  changeStateDropDown();
-}
-
-function setActiveClass(element) {
-  const prevSelectedOptions = document.querySelector('.select__option_selected');
-  if (prevSelectedOptions) {
-    prevSelectedOptions.classList.remove('select__option_selected');
+  function getOption() {
+    const listSelectOptions = document.querySelectorAll('.select__options');
+    listSelectOptions.forEach((selectOptions) => {
+      selectOptions.addEventListener('click', setOption);
+      selectOptions.addEventListener('click', (event) =>
+        updateSelectToggle(activelectToggle, event.target)
+      );
+      selectOptions.addEventListener('click', () => changeStateDropDown(activelectToggle));
+    });
   }
-  element.classList.add('select__option_selected');
-}
+
+  return {
+    getDropdownState,
+    getOption,
+  };
+})();
